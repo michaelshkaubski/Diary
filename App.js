98c055a728706eefdfadbdb164b74530e3dc173e
Michael Shkarubski <40541820+epamekids@users.noreply.github.com>
2019-01-04 20:56:05 +0300

    Addin firebase

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Form, Item, Label, Input, Textarea, Container, Header, Title, Content, Card, CardItem, Button, Right, Body, Text } from 'native-base';
-import { StyleSheet, Alert, Image, ImageBackground } from 'react-native'
+import { Form, Item, Label, Input, Textarea, Container, Header, Title, Content, Card, CardItem, Button, Right, Body } from 'native-base';
+import { StyleSheet, Alert, Image, ImageBackground, View } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import * as firebase from 'firebase'
 
 export default class AnatomyExample extends Component {
 
@@ -9,10 +10,21 @@ export default class AnatomyExample extends Component {
     super(props)
 
     this.state = {
+      id: 0,
       title: '',
       body: '',
       notes: [],
     }
+
+    var config = {
+      apiKey: "AIzaSyA8AE4HmcAtDP7WhLNIYJcTmHhvv6zwQ5M",
+      authDomain: "hinet-d1a28.firebaseapp.com",
+      databaseURL: "https://hinet-d1a28.firebaseio.com",
+      projectId: "hinet-d1a28",
+      storageBucket: "hinet-d1a28.appspot.com",
+      messagingSenderId: "676739768355"
+    };
+    firebase.initializeApp(config);
   }
 
   createNote() {
@@ -22,15 +34,11 @@ export default class AnatomyExample extends Component {
     noteList.push (
       <Card>
         <CardItem header>
-          <Text>
-            { this.state.title }
-          </Text>
+          { this.state.title }
         </CardItem>
         <CardItem>
           <Body>
-            <Text>
-              { this.state.body }
-            </Text>
+            { this.state.body }
           </Body>
         </CardItem>
       </Card>
@@ -46,19 +54,25 @@ export default class AnatomyExample extends Component {
       body:  <Form>
               <Textarea rowSpan={5} bordered placeholder="Body..." />
              </Form>,
-      notes: noteList
+      notes: noteList,
+      id: this.state.id++
     })
 
+    firebase.database().ref('Posts/' + this.state.title).set({
+      body: this.state.body
+    });
   }
 
   render() {
 
     return (
-      <ImageBackground source={{uri: 'https://images.pexels.com/photos/1420440/pexels-photo-1420440.jpeg?cs=srgb&dl=android-wallpaper-dawn-dusk-1420440.jpg'}} style={{width: '100%', height: '100%'}}>
-        <Header />
-        <Button onPress={() => this.createNote()} rounded style={styles.addButton}><Icon name="plus"/></Button>
-        <Text>{ this.state.notes }</Text>
-      </ImageBackground>
+      <View>
+        <ImageBackground source={{uri: 'https://images.pexels.com/photos/1420440/pexels-photo-1420440.jpeg?cs=srgb&dl=android-wallpaper-dawn-dusk-1420440.jpg'}} style={{width: '100%', height: '100%'}}>
+          <Header />
+          <Button onPress={() => this.createNote()} rounded style={styles.addButton}><Icon name="plus"/></Button>
+          { this.state.notes }
+          </ImageBackground>
+      </View>
     );
   }
 }
