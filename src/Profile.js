1188d31fa31ed880f2a54bccf3f881c32fee60fe
Michael Shkarubski <40541820+epamekids@users.noreply.github.com>
2019-01-21 21:59:08 +0300

    Adding image picker

--- src/Profile.js
+++ src/Profile.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import SignUp from './SignUp';
+import { ImagePicker } from 'expo'
 
 export default class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      image: null,
+    }
+  }
 
   render() {
     return (
       <View>
         <Text>{ this.props.email }</Text>
+        { this.state.image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} /> }
+        <Button
+          title="Choose avatar"
+          onPress={this._pickImage}
+        />
       </View>
     )
   }
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
 }
